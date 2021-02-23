695db370abd872483471efdb5458333ab993371f
SolomonChang <78829680+SolomonChang@users.noreply.github.com>
2021-02-24 12:51:00 +1300

    Add files via upload

---
+++ passwordmanager 1 test.py
@@ -0,0 +1,15 @@
+#Password manager programm
+#the programm creates and saves usernames and passwords
+#23/02/21
+
+name= ""
+age= ""
+
+print("Welcome to Password Manager!!")
+print("If you are 13 or older, you can store access details")
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+print("Hello", name)
+print("Sorry, you do not qualify to open an account")
+print("Choose a mode by entering the number: ")
+print("        1: Add passwords  2:View passwords  3: Exit")  
