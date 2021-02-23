5ca284d9d129b7b6d563f6298f5ffde0b257a652
SolomonChang <78829680+SolomonChang@users.noreply.github.com>
2021-02-23 20:57:02 +1300

    Create passwordmanager.py

---
+++ passwordmanager.py
@@ -0,0 +1,14 @@
+#Password manager programm
+#the programm creates and saves usernames and passwords
+#23/02/21
+
+name= ""
+age= ""
+
+print("Welcome to Password Manager!!")
+print("If you are 13 or older, you can store access details")
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+print("Hello", name)
+print("Choose a mode by entering the number: ")
+print("        1: Add passwords  2:View passwords  3: Exit")
